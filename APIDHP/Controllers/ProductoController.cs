fa91b96bee1e98d71a15798a0e4f24067534ad10
Diego <diego.razen@gmail.com>
2024-03-15 05:47:55 -0500

    Agregar archivos de proyecto.

---
+++ APIDHP/Controllers/ProductoController.cs
@@ -0,0 +1,199 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using APIDHP.Models;
+
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.AspNetCore.Cors;
+
+
+namespace APIDHP.Controllers
+{
+    [EnableCors("ReglasCors")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductoController : ControllerBase
+    {
+        private readonly string cadenaSQL;
+
+        public ProductoController(IConfiguration config)
+        {
+            cadenaSQL = config.GetConnectionString("CadenaSQL");
+        }
+
+        [HttpGet]
+        [Route("Lista")]
+        public IActionResult Lista()
+        {
+            List<Producto> lista = new List<Producto>();
+
+            try
+            {
+
+                using (var conexion = new SqlConnection(cadenaSQL))
+                {
+                    conexion.Open();
+                    var cmd = new SqlCommand("sp_lista_productos", conexion);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    using (var rd = cmd.ExecuteReader())
+                    {
+                        while (rd.Read())
+                        {
+                            lista.Add(new Producto
+                            {
+                                Id_Producto = Convert.ToInt32(rd["Id_Producto"]),
+                                Nombre = rd["Nombre"].ToString(),
+                                Descripcion = rd["Descripcion"].ToString(),
+                                Precio = Convert.ToDecimal(rd["Precio"])
+                            });
+                        }
+                    }
+                }
+
+                return StatusCode(StatusCodes.Status200OK, new { mensaje = "ok", response = lista });
+
+            }
+            catch (Exception error)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { mensaje = error.Message, response = lista });
+
+            }
+        }
+
+        [HttpGet]
+        [Route("Obtener/{idProducto:int}")]
+        public IActionResult Obtener(int idProducto)
+        {
+            List<Producto> lista = new List<Producto>();
+            Producto producto = new Producto();
+
+            try
+            {
+
+                using (var conexion = new SqlConnection(cadenaSQL))
+                {
+                    conexion.Open();
+                    var cmd = new SqlCommand("sp_lista_productos", conexion);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    using (var rd = cmd.ExecuteReader())
+                    {
+                        while (rd.Read())
+                        {
+                            lista.Add(new Producto
+                            {
+                                Id_Producto = Convert.ToInt32(rd["Id_Producto"]),
+                                Nombre = rd["Nombre"].ToString(),
+                                Descripcion = rd["Descripcion"].ToString(),
+                                Precio = Convert.ToDecimal(rd["Precio"])
+                            });
+                        }
+                    }
+                }
+
+                producto = lista.Where(item => item.Id_Producto == idProducto).FirstOrDefault();
+
+                return StatusCode(StatusCodes.Status200OK, new { mensaje = "ok", response = producto });
+
+            }
+            catch (Exception error)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { mensaje = error.Message, response = producto });
+
+            }
+        }
+        
+        [HttpPost]
+        [Route("Guardar")]
+        public IActionResult Guardar([FromBody] Producto objeto)
+        {
+            
+            try
+            {
+                using (var conexion = new SqlConnection(cadenaSQL))
+                {
+                    conexion.Open();
+                    var cmd = new SqlCommand("sp_guardar_producto", conexion);
+                    cmd.Parameters.AddWithValue("nombre", objeto.Nombre);
+                    cmd.Parameters.AddWithValue("descripcion", objeto.Descripcion);
+                    cmd.Parameters.AddWithValue("precio", objeto.Precio);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.ExecuteNonQuery();
+              
+                }
+
+                return StatusCode(StatusCodes.Status200OK, new { mensaje = "ok"});
+
+            }
+            catch (Exception error)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { mensaje = error.Message});
+
+            }
+        } 
+
+        [HttpPut]
+        [Route("Editar")]
+        public IActionResult Editar([FromBody] Producto objeto)
+        {
+            
+            try
+            {
+                using (var conexion = new SqlConnection(cadenaSQL))
+                {
+                    conexion.Open();
+                    var cmd = new SqlCommand("sp_editar_producto", conexion);
+                    cmd.Parameters.AddWithValue("id_Producto", objeto.Id_Producto == 0 ? DBNull.Value : objeto.Id_Producto);
+                    cmd.Parameters.AddWithValue("nombre", objeto.Nombre is null ?  DBNull.Value : objeto.Nombre);
+                    cmd.Parameters.AddWithValue("descripcion", objeto.Descripcion is null ? DBNull.Value :objeto.Descripcion);
+                    cmd.Parameters.AddWithValue("precio", objeto.Precio == 0 ? DBNull.Value : objeto.Precio);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.ExecuteNonQuery();
+              
+                }
+
+                return StatusCode(StatusCodes.Status200OK, new { mensaje = "Editado"});
+
+            }
+            catch (Exception error)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { mensaje = error.Message});
+
+            }
+        }
+        
+        [HttpDelete]
+        [Route("Eliminar/{idProducto:int}")]
+        public IActionResult Eliminar(int idProducto)
+        {
+            
+            try
+            {
+                using (var conexion = new SqlConnection(cadenaSQL))
+                {
+                    conexion.Open();
+                    var cmd = new SqlCommand("sp_eliminar_producto", conexion);
+                    cmd.Parameters.AddWithValue("id_Producto", idProducto);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.ExecuteNonQuery();
+              
+                }
+
+                return StatusCode(StatusCodes.Status200OK, new { mensaje = "Eliminado"});
+
+            }
+            catch (Exception error)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { mensaje = error.Message});
+
+            }
+        }
+    }
+
+}
+
---
+++ APIDHP/Models/Producto.cs
@@ -0,0 +1,13 @@
+﻿namespace APIDHP.Models
+{
+    public class Producto
+    {
+       
+            public int Id_Producto { get; set; }
+            public string Nombre { get; set; }
+            public string Descripcion { get; set; }
+            public decimal Precio { get; set; }
+
+   
+    }
+}
